be3ae66e933a492b01430550907d8dee2ec59398
Ananya Anand <ananya.anand.ir@gmail.com>
2026-08-25 18:35:03 +0530

    Update currencyconverter.py

--- currencyconverter.py
+++ currencyconverter.py
@@ -1,20 +1,20 @@
 #currency checker
 print("checking your money in indian rupess")
-print("$= u.s dollar ")
-print("W= korean won ")
-print("E= euro ")
-print("p= sterling pound ")
-print("make sure currency symbol is as mentioned and its sensitive to caps")
-sym=input("enter currency symbol $ W p E =")
+print('''$= u.s dollar
+         W= korean won
+         E= euro 
+         p= sterling pound''')
+print("make sure currency symbol is as mentioned")
+sym=input("enter currency symbol $ w p e =").lower()
 val=float(input("enter value="))
 if sym=="$":
     print("value in inr is ", val*95.68)
-elif sym=="W":
+elif sym=="w":
     print("value in inr is", val*0.069)
-elif sym=="E":
+elif sym=="e":
     print("value in inr is", val*111.76)
 elif sym=="p":
     print("value in inr is", val*130.52)
 else:
      print("unrecognised currency!")
-print("value is as per date 24/8/26")
+print("Currency exchange rate is as per date 24/8/26")
