5bfe135237e4a5c6c200e2516e8886471513b9dc
Ananya Anand <ananya.anand.ir@gmail.com>
2026-08-27 20:47:22 +0530

    Update password_scrambler.py

--- password_scrambler.py
+++ password_scrambler.py
@@ -1,8 +1,7 @@
 
 # test code for run
  
-ui =input("enter password=")
-ui =ui.lower()
+ui =input("enter password=").lower()
 leet_rules={"a":"4","e":"6","i":"9","o":"0","u":"8"}
 sp=""
 for letter in ui:
