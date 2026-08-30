47ff241eecf3f2ee7c0f0dc8c4e1dbdc1cf6a8d8
Ananya Anand <ananya.anand.ir@gmail.com>
2026-08-30 18:11:57 +0530

    Create passwordscrambler.py

---
+++ 01_password_scrambler/passwordscrambler.py
@@ -0,0 +1,11 @@
+# test code for run
+ 
+ui =input("enter password=").lower()
+leet_rules={"a":"4","e":"6","i":"9","o":"0","u":"8"}
+sp=""
+for letter in ui:
+    if letter in leet_rules:
+        sp=sp+leet_rules[letter]
+    else:
+        sp=sp+letter
+print("your secure password is:",sp) 
