c25e1db62bf0b4fe83bdec77752bc32f0d85febf
Ananya Anand <ananya.anand.ir@gmail.com>
2026-09-03 19:51:36 +0530

    Add currency converter functionality
    Implemented a currency converter that converts various currencies to Indian Rupees based on user input.

---
+++ currencyconverter/currency_converter.py
@@ -0,0 +1,20 @@
+#currency checker
+print("checking your money in indian rupess")
+print('''$= u.s dollar
+         W= korean won
+         E= euro 
+         p= sterling pound''')
+print("make sure currency symbol is as mentioned")
+sym=input("enter currency symbol $ w p e =").lower()
+val=float(input("enter value="))
+if sym=="$":
+    print("value in inr is ", val*95.68)
+elif sym=="w":
+    print("value in inr is", val*0.069)
+elif sym=="e":
+    print("value in inr is", val*111.76)
+elif sym=="p":
+    print("value in inr is", val*130.52)
+else:
+     print("unrecognised currency!")
+print("Currency exchange rate is as per date 24/8/26") 
